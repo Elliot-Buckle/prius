f76d44fc1df6089ccb2bc013031c06f0bb4ff558
Elliot-Buckle <elliotbuckle42@gmail.com>
2024-03-04 20:12:38 +0000

    discharge coeff added

--- engine.py
+++ engine.py
@@ -10,6 +10,7 @@ class Engine:
         mix_ratio:float,
         y:float,
         ox_den:float,
+        cd:float = 1,
         Pe:float = P_sl
     ):
         self.chamber_pressure = Pc
@@ -20,12 +21,7 @@ class Engine:
         self.mol_mass = M
         self.thrust = thrust
         self.ox_density = ox_den
-        
-        # funcs
-        def mass_flux(pressure):
-            return self.chamber_pressure*self.ratio_of_heats*np.sqrt(
-                (2/(self.ratio_of_heats + 1))**((self.ratio_of_heats + 1)/(self.ratio_of_heats - 1))
-            )/np.sqrt(self.ratio_of_heats*R*self.chamber_temp)
+        self.discharge_coeff = cd
         
         # calcs
         self.pressure_ratio = self.chamber_pressure/self.exit_pressure_1D
@@ -38,9 +34,15 @@ class Engine:
             2*self.ratio_of_heats/
             (self.mol_mass*(self.ratio_of_heats - 1))*R*self.chamber_temp*(1 - (self.exit_pressure_1D/self.chamber_pressure)**((self.ratio_of_heats - 1)/self.ratio_of_heats))
             )
-        #self.isp_ms = (self.exit_vel_1D*self.mass_flux())
+        #self.isp_m_s = (self.exit_vel_1D*self.mass_flux())
         
         self.ox_flow = self.thrust/self.exit_vel_1D
+        
+    # funcs
+    def mass_flux(self, pressure):
+        return self.chamber_pressure*self.ratio_of_heats*np.sqrt(
+            (2/(self.ratio_of_heats + 1))**((self.ratio_of_heats + 1)/(self.ratio_of_heats - 1))
+        )/np.sqrt(self.ratio_of_heats*R*self.chamber_temp)
 
 engine = Engine(Pc=2*10**6, Tc=3000, thrust=300, M=0.026, mix_ratio=8, y=1.2)
 print(engine.ox_flow)
