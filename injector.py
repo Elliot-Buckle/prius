0088b11b3feaee2a1c18d2507245fa6f4bed4a29
Henry Philp <K2233557@kingston.ac.uk>
2024-03-04 16:04:05 +0000

    code additions + edits

--- injector.py
+++ injector.py
@@ -1,12 +1,17 @@
 import numpy as np
 from numpy import *
 from constants import *
+from engine import Engine
 
 class Injector:
     
     def __init__(
+        self,
+        engine:Engine,
+    ):
+    
+        self.calculate()
         
         
+    def calculate(self):
         
-        
-    )
