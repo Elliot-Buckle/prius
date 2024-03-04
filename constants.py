ed4a0e1c913d5e8545250621adfcd8834ffd0edd
Henry Philp <K2233557@kingston.ac.uk>
2024-03-04 15:49:12 +0000

    created constants.py

---
+++ constants.py
@@ -0,0 +1,14 @@
+# Gravitational constant
+G = 6.67e-11
+
+# Earth gravitational acceleration (ms^-2)
+g = 9.80665
+
+# Universal gas constant 
+R = 8.31446261815324
+
+# Sea level pressure (Pa)
+P_sl = 101325.0
+
+# Ambient temperature at 20C (K)
+T_sl = 293.15
---
+++ injector.py
@@ -0,0 +1,12 @@
+import numpy as np
+from numpy import *
+from constants import *
+
+class Injector:
+    
+    def __init__(
+        
+        
+        
+        
+    )
