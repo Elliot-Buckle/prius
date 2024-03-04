63b50516d830880e613efba023b919bc72e19e0d
Henry Philp <K2233557@kingston.ac.uk>
2024-03-04 20:24:31 +0000

    more code

--- injector.py
+++ injector.py
@@ -14,4 +14,19 @@ class Injector:
         
         
     def calculate(self):
+        # Determine pressure drop
+        self.pressure_drop = 0.3 * Engine.pressure_chamber
+        print(self.pressure_drop)
+        
+        # Determine injection area
+        self.injection_area = Engine.ox_flow / (Engine.discharge_coefficient * sqrt(2 * self.pressure_drop * Engine.ox_density))
+        print(self.injection_area)
+        
+        # Determine injection velocity
+        self.injection_velocity = sqrt(2 * self.pressure_drop / Engine.ox_density)
+        print(self.injection_velocity)
+        
+        # Determine manifold pressure
+        self.pressure_manifold = Engine.pressure_chamber + self.pressure_drop
+        print(self.pressure_manifold)
         
