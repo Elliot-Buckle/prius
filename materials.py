7b28f9e9c94deb60c8688da5c4a06604b9f58b8a
Elliot-Buckle <elliotbuckle42@gmail.com>
2024-04-17 11:32:15 +0100

    fixed aluminium in mat

--- materials.py
+++ materials.py
@@ -1,5 +1,5 @@
 materials = {
-    "Aluminium":{"k_x":237, "k_y":237, "C":910, "rho":2710, "Tmax":390+273.15, "colour":"silver"},
+    "Aluminium":{"k_x":237, "k_y":237, "C":910, "rho":2710, "Tmax":300+273.15, "colour":"silver"},
     "HBN":{"k_x":16.8, "k_y":18.45, "C":1470, "rho":1985, "Tmax":850+273.15, "colour":"cornsilk"},
     "RCC":{"k_x":32, "k_y":40.3, "C":712, "rho":1450, "Tmax":650+273.15, "colour":"black"},
     "PG-SN":{"k_x":114, "k_y":400, "C":710, "rho":2190, "Tmax":650+273.15, "colour":"black"},
